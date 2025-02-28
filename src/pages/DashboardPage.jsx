0bf483fdb7c4d86902e3cd7f461c41be32caf0ab
fertejj <fernandojtejerina@gmail.com>
2025-02-28 13:52:11 -0300

    Agregue redireccion a home si no hay user

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import { useContext } from 'react'
+import { AuthContext } from '../context/AuthContext';
+import { Navigate } from 'react-router-dom';
 
 const DashboardPage = () => {
+  const { user } = useContext(AuthContext);
+  if (!user) {
+    return <Navigate to="/" />;
+  }
   return (
     <div className='bg-[#040823] h-[75vh] text-white'>DashboardPage</div>
   )
