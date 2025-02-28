244d33855aad395f75e4ea37119f62d2be77775f
fertejj <fernandojtejerina@gmail.com>
2025-02-28 15:58:16 -0300

    eliminacion total de chakra ui

--- src/components/dashboard/TradeList.jsx
+++ src/components/dashboard/TradeList.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import { Button, HStack } from "@chakra-ui/react"
 
 const TradeList = () => {
   return (
     <div>
         <div>Operaciones registradas:</div>
-        <HStack>
-      <Button>Click me</Button>
-      <Button>Click me</Button>
-    </HStack>
+
     </div>
   )
 }
