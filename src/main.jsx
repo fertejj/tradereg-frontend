180594919d6d928628f1b61952bd7eb489ec520c
Fer Tejerina <fertejerina97@gmail.com>
2025-03-27 15:02:13 -0300

    Cambie de lugar el AccountProvider hacia el Main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import { AccountProvider } from "./context/AccountContext.jsx";
 import "./index.css";
 import App from "./App.jsx";
-import { AuthProvider } from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
-
+      <AccountProvider>
         <App />
-
+      </AccountProvider>
     </AuthProvider>
   </StrictMode>
 );
